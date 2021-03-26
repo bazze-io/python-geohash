a4a741a7f01e423d7bc5d831a3dfcf012462357f
Jason Haas <jasonrhaas@gmail.com>
2021-03-26 10:52:02 -0500

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import Extension, setup
 
 setup(
     name="geohashlite",
-    version="0.3.1",
+    version="0.3.2",
     author="Xuzhou Qin",
     author_email="me@qinxuzhou.com",
     packages=["geohashlite"],
